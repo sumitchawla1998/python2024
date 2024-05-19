8666fb6caae615f6d1befb1ed7fab38902da1fbd
THESUMITSIR <sumitchawla1998@gmail.com>
2024-05-19 13:59:55 +0530

    add output statements folder

---
+++ PythonOutputStatements/printbiodata.py
@@ -0,0 +1,9 @@
+print("*****Biodata*****")
+print("First Name:Sumit")
+print("Last Name:Chawla")
+print("City:Anand")
+print("State:Gujarat")
+print("Country:India")
+
+
+# print list of your 5 favourite movies
