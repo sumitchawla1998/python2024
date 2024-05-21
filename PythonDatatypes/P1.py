b400261a789ec62bc2d2fc0c865d2a2eabcd733d
THESUMITSIR <sumitchawla1998@gmail.com>
2024-05-21 20:31:19 +0530

    add python datatypes folder

---
+++ PythonDatatypes/P1.py
@@ -0,0 +1,12 @@
+name = "Sumit"
+age = 30
+sal = 7000.90
+ismarried = False
+dob = None
+
+print("****Biodata*****")
+print("Name:",name)
+print("Age:",age)
+print("Salary:",sal)
+print("Married Status:",ismarried)
+print("DOB:",dob)
